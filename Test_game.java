029c25c7e5ac42a9afea3c0f7d34f5c7ae05c8b3
padmasahu <38810542+padmasahu@users.noreply.github.com>
2020-09-21 22:22:16 +0530

    Create Test_game.java

---
+++ Test_game.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Test_game{
+ 
+ 
+@Test
+public final void testGame() {
+        Game game = new Game();
+        Assert.assertEquals(1, game(1));
+        }
+ }
