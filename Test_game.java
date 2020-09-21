79adcd8f3b8585d21db46cc12489ce1922e4d58d
padmasahu <38810542+padmasahu@users.noreply.github.com>
2020-09-21 22:46:20 +0530

    Update Test_game.java

--- Test_game.java
+++ Test_game.java
@@ -7,10 +7,10 @@ public class Test_game{
 @Test
 public final void testGame() {
         Game game = new Game();
-        Assert.assertEquals(1, game(1));
-        Assert.assertEquals(2, game(2));
-        Assert.assertEquals("Fizz", game(3));
-        Assert.assertEquals("Buzz", game(5));
-        Assert.assertEquals("FizzBuzz", game(15));
+       int[][] testCases = { { 1, 1 }, { 2, 2 }, { 3, 	Fizz}, { 4, 4 }, { 5, Buzz }, { 5, 5 }, { 6, Fizz}, { 7, 7 },
+			{ 8, 8 }, { 9, 9 }, { 10, Buzz } };
+	for (int i = 0; i < testCases.length; i++) {
+		Assert.assertEquals(testCases[i][1], game(testCases[i][0]));
+	}
         }
  }
