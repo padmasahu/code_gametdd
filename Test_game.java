1658106b4ec552de4fbe18702eff3296803b8263
padmasahu <38810542+padmasahu@users.noreply.github.com>
2020-09-21 22:44:21 +0530

    Update Test_game.java

--- Test_game.java
+++ Test_game.java
@@ -9,5 +9,8 @@ public final void testGame() {
         Game game = new Game();
         Assert.assertEquals(1, game(1));
         Assert.assertEquals(2, game(2));
+        Assert.assertEquals("Fizz", game(3));
+        Assert.assertEquals("Buzz", game(5));
+        Assert.assertEquals("FizzBuzz", game(15));
         }
  }
