45b586b4243fb28e2966b6e9c8ecd88ba1cbe94d
padmasahu <38810542+padmasahu@users.noreply.github.com>
2020-09-21 22:53:23 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,14 +1,17 @@
 class Game{
     static String game(int n){
-        if(n==1)
-         return 1;
-        if(n==2)
-            return 2;
-        if(n==3)
-            return "Fizz";
-        if(n==5)
-            return "Buzz";
-        if(n==15)
+        if(n%15==0){
             return "FizzBuzz";
+        }
+        else if(n%3==0){
+            return "Fizz";
+        }
+        else if(n%5==0){
+            return "Buzz";
+        }
+        else{
+           String str = String.valueOf(n);
+            return str;
+        }
     }
 }
